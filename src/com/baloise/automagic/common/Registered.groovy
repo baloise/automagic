2e0e4be6487dd06f6ba3f4ea811380b5c630ca39
Matthias Cullmann <matthias.cullmann@baloise.com>
2021-12-16 11:53:48 +0100

    delegate unknown methods and properties from services to CpsScript

--- src/com/baloise/automagic/common/Registered.groovy
+++ src/com/baloise/automagic/common/Registered.groovy
@@ -1,5 +1,6 @@
 package com.baloise.automagic.common
 
+import com.cloudbees.groovy.cps.NonCPS
 
 // yes, I know ... composition over inheritance, 
 // but traits don't work in Jenkins (https://issues.jenkins.io/browse/JENKINS-46145) 
@@ -7,6 +8,18 @@ package com.baloise.automagic.common
 abstract class Registered implements Serializable, Constructed<Registered, Object> {
 	protected Registry registry
 	protected def steps
+	
+	@NonCPS
+	def propertyMissing(String name) { steps.getProperty(name) }
+	
+	@NonCPS
+	def methodMissing(String name, args) {
+		steps.invokeMethod(name,args)
+	}
+	
+	def sleep(int seconds) {
+		steps.sleep(seconds)
+	}
 	
 	@Override
 	public Registered construct(Object params = null) {
--- src/com/baloise/automagic/magic/internal/MagicImpl.groovy
+++ src/com/baloise/automagic/magic/internal/MagicImpl.groovy
@@ -15,89 +15,89 @@ class MagicImpl extends Registered implements MagicService {
 	
 	@Override
 	def magic() {
-		steps.echo "using automagic v"+ Automagic.VERSION
+		echo "using automagic v"+ Automagic.VERSION
 		
-		if(steps.env.NODE_NAME) steps.error """Automagic is a potentially long running pipeline. 
-		You must not run inside a node but are running on '${steps.env.NODE_NAME}'. 
+		if(env.NODE_NAME) error """Automagic is a potentially long running pipeline. 
+		You must not run inside a node but are running on '${env.NODE_NAME}'. 
 		For declarative pipelines please use 'agent none'. 
 		For scripted pipelines make sure you are not running inside a 'node(){}'."""
 		
 		// we do the following in two loops to avoid sleeping inside the node
 		Map<String, String> yamls
-		steps.node(''){
-			steps.checkout steps.scm
-			yamls = steps.findFiles(glob: '.automagic/**/*.yaml').collectEntries{["${it.path}": steps.readYaml(file: it.path)]}
+		node(''){
+			checkout scm
+			yamls = findFiles(glob: '.automagic/**/*.yaml').collectEntries{["${it.path}": readYaml(file: it.path)]}
 		}
-		yamls.each{ name,yaml -> 
+		yamls.each{ name,yaml ->
 			doMagic(name, yaml)
 		}
 	}
 	
 	def doMagic(name, yaml) {
-		steps.echo "applying $name" 
+		echo "applying $name" 
 		String ip
 				
 		String ComputerName = props.get('ComputerName-'+yaml.spec.id)
-		if(ComputerName) steps.echo "ComputerName is $ComputerName"
+		if(ComputerName) echo "ComputerName is $ComputerName"
 	
 		def json = oim.getVMDetails(ComputerName)
 		ip = json.Status == 'Success' ? json.Result[0].PrimaryIP : ''
-		if(ip) steps.echo "IP is $ip"
+		if(ip) echo "IP is $ip"
 		
 		String jiraTask = props.get('JIRA-Decommission-ID-'+ComputerName)
 		
 		if(yaml.spec.status == "decommissioned") {
-			steps.echo("Starting Decommissioning procedure")
+			echo("Starting Decommissioning procedure")
 			assert ComputerName
 			if(jiraTask) {
-				steps.error "JIRA issue for decommissioning already exists: " + jiraTask
+				error "JIRA issue for decommissioning already exists: " + jiraTask
 			}
-			steps.echo("Scheduling " + ComputerName + " for decommissioning.")
+			echo("Scheduling " + ComputerName + " for decommissioning.")
 			createDecommissionJiraTask(ComputerName,yaml.metadata.serviceOwner)
 			return
 		}
 	
 		if(yaml.spec.status == "active") {
 			if(jiraTask) {
-				steps.error "JIRA issue for decommissioning exists: " + jiraTask
+				error "JIRA issue for decommissioning exists: " + jiraTask
 			} 
 			if(!ComputerName || json.Status != 'Success'){
-				 steps.echo "Creating VM"
+				 echo "Creating VM"
 				 String changeNo = "CR"+java.util.UUID.randomUUID()
-				 steps.echo "changeNo = $changeNo"
-				 String req_body = steps.libraryResource(resource:'mycloud/createVM.json').replaceAll("<changenumber>", changeNo)
+				 echo "changeNo = $changeNo"
+				 String req_body = libraryResource(resource:'mycloud/createVM.json').replaceAll("<changenumber>", changeNo)
 				 yaml.spec.each{ key, value ->
 				 req_body = req_body.replaceAll("<$key>", "$value")
 				 }
 				 json = oim.createVM(req_body)
 				 assert json.Status == 'Success'
 				 int request_no = json.Result
-				 steps.echo "Request number is $request_no"
+				 echo "Request number is $request_no"
 	
 				 // All my dreams fulfil
 				 while(true) {
 					 json = oim.getRequest(request_no)
 					 assert json.Status == 'Success'
-					 steps.echo "RequestStatus = " + json.Result[0].RequestStatus
+					 echo "RequestStatus = " + json.Result[0].RequestStatus
 					 if(json.Result[0].RequestStatus == "Fulfilment Completed") break
-					 steps.echo "sleeping 37 seconds"
-					 steps.sleep(37)
+					 echo "sleeping 37 seconds"
+					 sleep(37)
 				 }
 	
 				 json = oim.getAllCIDetails(request_no)
 				 assert json.Status == 'Success'
 				 ip = json.Result[0].PrimaryIP
 				 ComputerName = json.Result[0].ComputerName
-				 steps.echo "ComputerName is $ComputerName"
-				 steps.echo "IP is $ip"
+				 echo "ComputerName is $ComputerName"
+				 echo "IP is $ip"
 				 props.put('ComputerName-'+yaml.spec.id, ComputerName)
 			}
-			steps.node(''){
-				steps.sh "ssh -o BatchMode=true -o ConnectTimeout=7 -o StrictHostKeyChecking=no $ip"
+			node(''){
+				sh "ssh -o BatchMode=true -o ConnectTimeout=7 -o StrictHostKeyChecking=no $ip"
 			}
 			return
 		}
-		steps.error "invalid status: ${yaml.spec.status}"
+		error "invalid status: ${yaml.spec.status}"
 	}
 
 	
@@ -105,14 +105,14 @@ class MagicImpl extends Registered implements MagicService {
 		//TODO move this to OIM
 		// perhaps add skill-layer when clarified what to use.
 		if(!host || !serviceOwner) {
-			steps.error "createDecommissionJiraTask: invalid arguments"
+			error "createDecommissionJiraTask: invalid arguments"
 		}
 	
 		registry.getService(CredentialsService).withCredentials('JIRA',
 			['USERNAME', 'PASSWORD']
 	  ) {
 		  
-		  def res = steps.readJSON(text: steps.httpRequest(acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: true, name: 'Authorization', value: 'Basic '+Base64.encoder.encodeToString("${steps.JIRA_USERNAME}:${steps.JIRA_PASSWORD}".bytes)]], httpMode: 'POST', requestBody: """{
+		  def res = readJSON(text: httpRequest(acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: true, name: 'Authorization', value: 'Basic '+Base64.encoder.encodeToString("${JIRA_USERNAME}:${JIRA_PASSWORD}".bytes)]], httpMode: 'POST', requestBody: """{
      "fields": {
      "project": {
        "id": "56280"
